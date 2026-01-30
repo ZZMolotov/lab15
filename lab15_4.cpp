60eb0bc3077f4b54e0976b278be0f7f756c570c3
ZZMolotov <zzmolotov8559@gmail.com>
2026-01-30 16:33:04 +0700

    lab15_4

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,14 @@ int main(){
 }
 
 //Do not modify source code above this line
+
+void shuffle(int &a, int &b, int &c, int&d){
+	int *num[4]= {&a, &b, &c, &d}; 
+	for(int i = 3; i>0; i--){
+		int j = rand()%i;
+
+		int temp = *num[i];
+		*num[i]=*num[j];
+		*num[j]= temp;
+	}
+}
